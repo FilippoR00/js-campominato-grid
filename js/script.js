2bf7f065469cf682e9ac09de467f94e1f0f0f109
Filippo Rossi <rossifilippo2000@gmail.com>
2021-11-17 17:10:04 +0100

    added safe squares

--- js/script.js
+++ js/script.js
@@ -2,42 +2,53 @@ let difficulty = document.getElementById("difficulty");
 let content = "";
 let box = document.querySelector(".box");
 
+
 function getSquares(value) {
     if (value == "easy") {
         content = "";
-        for (let i = 0; i < 100; i++) {
+        for (let i = 1; i <= 100; i++) {
             content += 
             `
-            <div class="square easy"></div>
+            <div class="square easy">${i}</div>
             `
         }
     }
     if (difficulty.value == "hard") {
         content = "";
-        for (let i = 0; i < 81; i++) {
+        for (let i = 1; i <= 81; i++) {
             content +=
             `
-            <div class="square hard"></div>
+            <div class="square hard">${i}</div>
             `
         }
     }
     if (difficulty.value == "extreme") {
         content = "";
-        for (let i = 0; i < 49; i++) {
+        for (let i = 1; i <= 49; i++) {
             content +=
                 `
-            <div class="square extreme"></div>
+            <div class="square extreme">${i}</div>
             `
         }
     }
     box.innerHTML = content;
 }
 
-
+function click(square){
+    for (let i = 0; i < square.length; i++) {
+        square[i].addEventListener("click", function () {
+            square[i].classList.add("safe");
+        })
+    }
+}
 
 
 document.getElementById("btn").addEventListener("click", function() {
     getSquares(difficulty.value);
-
-
+    let square = document.getElementsByClassName("square");
+    click(square);
+    
+    
 })
+
+
